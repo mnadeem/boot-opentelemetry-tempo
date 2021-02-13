1dcbf4baed5e19ce510408d4e83091717c5faa3e
Nadeem Mohammad <nadeembtech@gmail.com>
2021-02-13 23:38:27 +0530

    more changes

--- boot-otel-tempo-provider1/db/scripts/init.sql
+++ boot-otel-tempo-provider1/db/scripts/init.sql
@@ -6,13 +6,11 @@ CREATE TABLE flight (
   origin CHAR(3) NOT NULL,
   destination CHAR(3) NOT NULL,
   airline VARCHAR(40) NOT NULL,
-  departing timestamp NOT NULL
-);
+  departing timestamp NOT NULL);
 
 INSERT INTO
 	flight(origin, destination, airline, departing)
 VALUES
   ('SEA', 'LAS', 'Deltoid', '2020-12-01 12:10:00'),
   ('SEA', 'LAS', 'Unitely', '2020-12-01 13:25:00'),
-  ('SEA', 'LAS', 'Pan-Um', '2020-12-01 09:15:00')
-;
+  ('SEA', 'LAS', 'Pan-Um', '2020-12-01 09:15:00');
