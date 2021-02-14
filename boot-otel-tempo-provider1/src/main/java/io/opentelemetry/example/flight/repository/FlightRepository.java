172f9483b8b1f1e2b8d978994b77ad25ae9e377b
Nadeem, Mohammad <coolmind182006@gmail.com>
2021-02-14 07:50:11 +0530

    @Repository

--- boot-otel-tempo-provider1/src/main/java/io/opentelemetry/example/flight/repository/FlightRepository.java
+++ boot-otel-tempo-provider1/src/main/java/io/opentelemetry/example/flight/repository/FlightRepository.java
@@ -1,7 +1,9 @@
 package io.opentelemetry.example.flight.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FlightRepository extends CrudRepository<Flight, Long> {
 
 }
