bea795019e8e526e8534ee2f84987a212ec1d449
Nadeem, Mohammad <mnadeem@optum.com>
2021-02-14 08:12:06 +0530

    more changes

--- boot-otel-tempo-provider1/src/main/java/io/opentelemetry/example/config/Provider1DbConfig.java
+++ boot-otel-tempo-provider1/src/main/java/io/opentelemetry/example/config/Provider1DbConfig.java
@@ -10,6 +10,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -24,32 +25,37 @@ import com.zaxxer.hikari.HikariDataSource;
 @EnableJpaRepositories(entityManagerFactoryRef = "provider1EntityManagerFactory", transactionManagerRef = "provider1TransactionManager", basePackages = {"io.opentelemetry.example.flight" })
 public class Provider1DbConfig {
 	
+	@Primary
 	@Bean(name = "provider1DataSourceProperties")
 	@ConfigurationProperties("provider1.datasource")
 	public DataSourceProperties dataSourceProperties() {
 		return new DataSourceProperties();
 	}
-
+	
+	@Primary
 	@Bean(name = "provider1DataSource")
 	@ConfigurationProperties("provider1.datasource.configuration")
 	public HikariDataSource dataSource(@Qualifier("provider1DataSourceProperties") DataSourceProperties properties) {
 		return properties.initializeDataSourceBuilder().type(HikariDataSource.class).build();
 	}
-
+	
+	@Primary
 	@Autowired
 	@Bean(name = "provider1NamedParamJdbcTemplate")
 	public NamedParameterJdbcTemplate cdrNamedParamJdbcTemplate(
 			@Qualifier("provider1DataSource") DataSource dataSource) {
 		return new NamedParameterJdbcTemplate(dataSource);
 	}
-
+	
+	@Primary
 	@Bean(name = "provider1EntityManagerFactory")
 	public LocalContainerEntityManagerFactoryBean entityManagerFactory(EntityManagerFactoryBuilder builder,
 			@Qualifier("cdrDataSource") DataSource dataSource) {
 		return builder.dataSource(dataSource).packages("io.opentelemetry.example.flight").persistenceUnit("provider1").build();
 
 	}
-
+	
+	@Primary
 	@Bean(name = "provider1TransactionManager")
 	public PlatformTransactionManager transactionManager(
 			@Qualifier("provider1EntityManagerFactory") EntityManagerFactory entityManagerFactory) {
